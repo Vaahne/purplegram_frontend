fa86191ba5652893531f8c14488c7b2aee521f61
Spring -Git <vaahne@yahoo.com>
2025-06-12 14:15:26 -0600

    import

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -3,7 +3,6 @@ import { useAuth } from "../context/authContext/auth";
 import AuthPage from "../pages/AuthPage";
 import NavBar from "./NavBar/NavBar";
 import SideNav from "./SideNav/SideNav";
-import WeatherApi from "./weatherApi/weatherApi";
 
 export default function ProtectedRoutes(){
     const {cookies} = useAuth();
